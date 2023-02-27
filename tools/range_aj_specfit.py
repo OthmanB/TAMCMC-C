afae54bb90eb43087a6fe73f39587847acba101f
Othman Benomar <othman.benomar@gmail.com>
2023-02-27 10:45:54 +0900

    Update range_aj_specfit.py
    minmax_alt() added in tools/range_aj_specfit.py to determine ranges of aj in a more sensible way than mimax()

--- tools/range_aj_specfit.py
+++ tools/range_aj_specfit.py
@@ -1,3 +1,4 @@
+import numpy as np
 '''
 	A tiny function that allows you to give an estimate to the
 	the slope and the ordinate at orgin for models fitting
@@ -12,11 +13,23 @@
 	   For example, if you want the range that is associated to values of aj varying 
 	   from 5*a_avg and a_avg/5, then n=5 
 '''
-import numpy as np
 def minmax(nu1, nu2, a_avg, n):
 	k1=a_avg*(n-1./n)/(nu1-nu2)
 	k2=-k1
 	b_sols=[n*a_avg- k1*nu1, n*a_avg- k1*nu2, n*a_avg- k2*nu1, n*a_avg- k2*nu2]
+	k_sols=[k1,k2]
+	print(' solutions of linear type: a = k nu + b')
+	print('slope range: [ {} , {} ]'.format(np.min(k_sols), np.max(k_sols)))
+	print('ordinate @ origin range: [ {} , {} ]'.format(np.min(b_sols), np.max(b_sols)))
+
+
+'''
+	Same as minmax(), but with a direct range of a1,a2 provided
+'''
+def minmax_alt(nu1, nu2, a1, a2):
+	k1=(a1-a2)/(nu1-nu2)
+	k2=-k1
+	b_sols=[a1- k1*nu1, a1- k1*nu2, a2- k2*nu1, a2- k2*nu2]
 	k_sols=[k1,k2]
 	print(' solutions of linear type: a = k nu + b')
 	print('slope range: [ {} , {} ]'.format(np.min(k_sols), np.max(k_sols)))
